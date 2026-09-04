a7fc5c9fff44e50a13668f2e5246d07abb4e72c6
ghostkingorg-lab <a.ghost.king.org.a@gmail.com>
2026-09-05 00:58:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").onclick = function() {
+    alert("Переходи в телеграмм! Поспеши!");
+};
+document.getElementById("btn2").onclick = function() {
+    alert("Если кто-то сможет поддержать автора в телеграмм, любым подарком, знайте, что благодаря вам улучшается качество любого проэкта!");
+};
