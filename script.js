9d4ddeeac803b00587c39a0fd3f6ea9a0fab998e
ghostkingorg-lab <a.ghost.king.org.a@gmail.com>
2026-09-05 11:36:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-document.getElementById("btn").onclick = function() {
-    alert("Переходи в телеграмм! Поспеши!");
 };
 document.getElementById("btn2").onclick = function() {
-    alert("Если кто-то сможет поддержать автора в телеграмм, любым подарком, знайте, что благодаря вам улучшается качество любого проэкта!");
+    alert("Истории в разработке!");
+};
+function toggleMusicMenu() {
+    var modal = document.getElementById("musicModal");
+    if (modal.style.display === "block") {
+        modal.style.display = "none";
+    } else {
+        modal.style.display = "block";
+    }
+}
+function playTrack(trackUrl, element) {
+    var audio = document.getElementById("mainAudio");
+    var items = document.querySelectorAll(".playlist li");
+    items.forEach(function(item) {
+        item.classList.remove("active");
+    });
+    element.classList.add("active");
+    audio.src = trackUrl;
+    audio.play();
+}
+window.onclick = function(event) {
+    var modal = document.getElementById("musicModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 };
